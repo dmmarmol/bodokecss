a8f5c5b16b546855d339fdab3a7990a8f9f479d4
Diego (MacBook) <diego@convoy.me>
2014-09-05 19:34:51 -0300

    animations, input, list, helpers

---
+++ js/bdk-lightbox.js
@@ -0,0 +1,50 @@
+// bodoke.lightbox.js
+
+var lighbox_open;
+var element = $('#lightbox');
+
+
+function openlbox(id) {
+	$('body').toggleClass('lightbox-active');
+	$(element'-'+id).fadeIn(function(){
+		lighbox_open = true;		
+	});
+}
+function closelbox() {
+	$('.lightbox').fadeOut(function(){
+		lighbox_open = false;
+		// $('.blackout').fadeOut();
+		$('body').removeClass('lightbox-active');
+	});
+}
+$(document).ready(function(){
+
+	// $('.openlbox').click(bindlbox);
+	$('*[lightbox]').click(function(){
+		var id = $(this).attr('lightbox');
+		openlbox( id );
+	});
+
+	// Close Button
+	$('.lightbox .close').click(function(e){
+		closelbox();
+	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27 && lighbox_open) { 
+			closelbox();
+		} // esc
+	});
+	// Outside Close
+	var mouse_is_outside;
+	$('.lightbox').hover(function(){ 
+		mouse_is_outside = false;
+	}, function(){ 
+		mouse_is_outside = true;
+	});
+	$("#blackout").click(function(){ 
+		if (mouse_is_outside) {
+			closelbox();
+		}
+	});
+
+});
