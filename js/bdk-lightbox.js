7ad386c6ae8383bc8c009fc783201b783ecd3109
Diego (MacBook) <diego@convoy.me>
2015-05-29 11:11:14 -0300

    update bdk-lightbox

--- js/bdk-lightbox.js
+++ js/bdk-lightbox.js
@@ -1,30 +1,58 @@
-// bdk-lightbox.js
+/**
+ * bdk-lightbox.js
+ * @constructor
+ * @desc Open and closes the lightbox
+ * @example
+ * @group bdk-lightbox
+ * @param {string} [list_element=.star-list'] - DOM element that holds the rating elements (stars)
+ * @return {void}
+ */
 
-var lightbox_open,
+var bdk_lightbox_open,
 	mouse_is_outside;
 
 var bdk_lightbox = 'bdk-lightbox';
+var bdk_lightbox_anim_class = 'fadeInUp';
 var bdk_lightbox_open_class = bdk_lightbox+'-active';
 var bdk_blackout = '#bdk-blackout';
 
 function openlbox(id) {
 	$('body').toggleClass(bdk_lightbox_open_class);
 	$('#'+bdk_lightbox+'-'+id).fadeIn(function(){
-		lightbox_open = true;		
-	});
+		bdk_lightbox_open = true;		
+	});//.addClass(bdk_lightbox_anim_class);
 }
 function closelbox() {
 	$('.'+bdk_lightbox).fadeOut(function(){
-		lightbox_open = false;
+		bdk_lightbox_open = false;
 		
 		$(bdk_blackout).fadeOut(function(){
+			// $(this).removeClass(bdk_lightbox_anim_class);
 			$('body').removeClass(bdk_lightbox_open_class);
 			$(this).removeAttr('style');
 		});
 	});
 }
-$(document).ready(function(){
+/**
+ * processlbox()
+ * This function is going to be trigger before the 'lightbox' is closing
+ * @param {string} 
+ * @see closelbox()
+ * @return {void}
+ */
+function processlbox(id) {
+	var alert = document.createElement('div');
+	var alertIcon = '<i class="ficon fa-circle-o-notch"></i>';
+	var alertMessage = '<span class="text">Procesando...</span>';
+		
+	alert.className += ' alert panel alert-quiet h-center';
+	alert.innerHTML = alert.innerHTML + alertIcon + alertMessage;
 
+	$('#'+bdk_lightbox+'-'+id).prepend(alert);
+}
+
+
+$(document).ready(function(){
 	// $('.openlbox').click(bindlbox);
 	$('*[lightbox]').click(function(){
 		var id = $(this).attr('lightbox');
