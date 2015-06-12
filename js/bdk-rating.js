28ac972d589743615f6e64fda65d5e7a5e3e90d3
Diego (MacBook) <diego@convoy.me>
2015-06-12 19:12:38 -0300

    Update ranking

--- js/bdk-rating.js
+++ js/bdk-rating.js
@@ -3,22 +3,25 @@
  * @constructor
  * @desc Fires the rating UI. Each parameter could recieve 'undefined' as a parameter.
  * @example
- * <ul class="star-list list list-inline-block" data-rating="{{ section_data.rating.votes }}">
- *	<li class="item" id="star-x">
- *		<label for="star-x-input">
- *			<i class="ficon fa-star star-full"></i>
- *			<i class="ficon fa-star-o star-empty"></i>
- *		</label>
- *		<input type="radio" class="hidden" id="star-x-input" name="rating" value="1" />
- *	</li>			
- *	<li class="item" id="star-x">
- *		<label for="star-x-input">
- *			<i class="ficon fa-star star-full"></i>
- *			<i class="ficon fa-star-o star-empty"></i>
- *		</label>
- *		<input type="radio" class="hidden" id="star-x-input" name="rating" value="1" />
- *	</li>			
- * </ul>
+ * <div class="post-rating">
+ *	<ul class="star-list list list-inline-block" data-rating="{{ section_data.rating.votes }}">
+ *		<li class="item" id="star-x">
+ *			<label for="star-x-input">
+ *				<i class="ficon fa-star star-full"></i>
+ *				<i class="ficon fa-star-o star-empty"></i>
+ *			</label>
+ *			<input type="radio" class="hidden" id="star-x-input" name="rating" value="1" />
+ *		</li>			
+ *		<li class="item" id="star-x">
+ *			<label for="star-x-input">
+ *				<i class="ficon fa-star star-full"></i>
+ *				<i class="ficon fa-star-o star-empty"></i>
+ *			</label>
+ *			<input type="radio" class="hidden" id="star-x-input" name="rating" value="1" />
+ *		</li>			
+ *	</ul>
+ *	<span class="post-votes"><!-- Votes count --></span>
+ * </div>
  * @see _rating.scss
  * @param {string} [list_element=.star-list'] - DOM element that holds the rating elements (stars)
  * @param {string} [single_element=.star-list .item'] - DOM element that represents a single rating element (star)
@@ -34,41 +37,57 @@ function bdk_rating(list_element, single_element, hover_class, half_class) {
 
 	var list = $(list_element);
 	var element = $(single_element);
-	var initial_rating = $(list_element).attr('data-rating');
-		initial_rating = initial_rating.replace(/\,/g,'.'); // Look for commas inside the number and replace them with a dot
-		initial_rating = Number(initial_rating);
+	var initial_rating;
+	// console.log(initial_rating);
+	var votes_count;
 
 	/**
 	 * If the mouse hover any of the single_element
 	 * then it will change his appearence 'filling' themselves
-	 *
+	 * - EXCEPT - if the user has voted before, then the list_element
+	 * will have a class named "blocked" in order to prevent the UI
 	 */
-	element.hover(
-		// MouseEnter
-		function(){
-			// var $elements = $(this).siblings('.item').andSelf();
-			var id = $(this).index();
-			element.removeClass(hover_class+' '+half_class);
-			element.slice(0, id + 1).addClass(hover_class);
-		},
-		// MouseOut
-		function(){
-			element.removeClass(hover_class+' '+half_class);
+	if ( !list.hasClass('blocked') ) {
+		element.hover(
+			// MouseEnter
+			function(){
+                votes_count = $('.post-rating .post-votes').text();
+				// var $elements = $(this).siblings('.item').andSelf();
+				var id = $(this).index();
+				// console.log('id: '+id);
+				element.removeClass(hover_class+' '+half_class);
+				element.slice(0, id + 1).addClass(hover_class);
 
-			var integer = Math.floor(initial_rating);
-			var decimal = initial_rating - integer;
-				decimal = (Math.round(decimal*2)/2).toFixed(1);
+				$('.post-rating .post-votes').text( $(this).attr('title') );
+			},
+			// MouseOut
+			function(){
+                initial_rating = $(list_element).attr('data-rating');
+                initial_rating = initial_rating.replace(/\,/g,'.'); // Look for commas inside the number and replace them with a dot
+                initial_rating = Number(initial_rating);
 
-			if ( decimal > 0 ) {
-				// Hover items plus a half one
-				element.slice(0, integer).addClass(hover_class);
-				element.eq(integer).addClass(half_class);
-			} else {
-				// Hover full stars
-				element.slice(0, integer).addClass(hover_class);
+				element.removeClass(hover_class+' '+half_class);
+
+				var integer = Math.floor(initial_rating);
+				var decimal = initial_rating - integer;
+					decimal = (Math.round(decimal*2)/2).toFixed(1);
+
+				// console.log( integer +' '+ decimal);
+
+				if ( decimal > 0 ) {
+					// Hover items plus a half one
+					element.slice(0, integer).addClass(hover_class);
+					element.eq(integer).addClass(half_class);
+				} else {
+					// Hover full stars
+					element.slice(0, integer).addClass(hover_class);
+				}
+
+                if ($('.post-rating .post-votes').text().indexOf("voto") == -1)
+				    $('.post-rating .post-votes').text(votes_count);
 			}
-		}
-	);
+		);		
+	}
 
 	/**
 	 * If the mouse leaves the list_element that contains all the single_element
